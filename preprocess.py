696abca6e8ae89200933fc311751ceb496c9d789
10-zin <33179372+10-zin@users.noreply.github.com>
2019-01-02 19:28:05 +0530

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -4,15 +4,22 @@ def divide_chunks(list , batch_size):
     for i in range(0, len(list), batch_size):
         yield list[i: i + batch_size]
 
-
-if __name__ == '__main__':
+def chunked_list(batch_size = 32):
     chunked_list = []
-    batch_size = 32
 
     imageList,AnnotationList = loader.call()
-    print(len(imageList), len(AnnotationList))
-
-
     chunked_list = list(divide_chunks(imageList, batch_size))
-    print(len(chunked_list))
-    print(chunked_list[0])
+    return chunked_list
+
+# TESTING:
+# if __name__ == '__main__':
+#     chunked_list = []
+#     batch_size = 32
+
+#     imageList,AnnotationList = loader.call()
+#     print(len(imageList), len(AnnotationList))
+
+
+#     chunked_list = list(divide_chunks(imageList, batch_size))
+#     print(len(chunked_list))
+#     print(chunked_list[0])
