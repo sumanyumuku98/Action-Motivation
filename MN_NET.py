c777b1bd0b784422e113eea290ff5b05b311c337
Sumanyu Muku <mukusumanyu@gmail.com>
2019-01-14 15:33:00 +0530

    Updated train function

--- MN_NET.py
+++ MN_NET.py
@@ -5,62 +5,106 @@ import os
 import torch.autograd
 import torch.optim as optim
 import torch.nn.functional as F 
-from torchvision import models
+from torchvision import models, transforms
+import sys
+import sys.path
+from torch.nn.utils.rnn import pack_padded_sequence 
+#import nltk
+#from nltk.corpus import stopwords
+#from nltk import tokenize
+
+MODEL_PATH = ''
 
 
 
 class MN_NET(torch.nn.Module):
-    def __init__(self, batch_size, seq_len=1,num_layers=2,hidden_size = 2, action_categories=0):
+    def __init__(self, batch_size, seq_len=1,hidden_size = 2,embed_size = 1,vocab_size = 1, action_categories=0):
         super(MN_NET, self).__init__()
         self.batch_size = batch_size
         self.hidden_size = hidden_size
 
-        self.num_layers = num_layers
+        self.embed_size = embed_size
+        self,vocab_size = vocab_size
+        #self.num_layers = num_layers
         self.categories = action_categories
-        self.seq_len = seq_len
+        
+        self.max_seq_len = seq_len
         self.resnet = models.resnet101(pretrained = True)
-        #z = torch.rand([1,3,224,224])
         modules=list(self.resnet.children())[:-2]
         self.resnet=nn.Sequential(*modules)
         for p in self.resnet.parameters():
             p.requires_grad = False
-        self.linear_layer = torch.nn.Linear(in_features = 2048*7*7, out_features = 2048)
-        self.dropout = torch.nn.Dropout(0.4)
-        self.linear_layer_two = torch.nn.Linear(in_features = 2048,out_features =256)
-        self.linear_classifier = torch.nn.Linear(in_features = self.num_layers*self.hidden_size, out_features= self.categories)
-        self.gru = torch.nn.GRU(input_size = 256,hidden_size = 256,num_layers = 2)
-        
-        
-            
+       
+        self.linear_layer = nn.Linear(in_features = 2048*7*7, out_features = 2048)
+        self.dropout = nn.Dropout(0.4)
+        self.linear_layer_embed = nn.Linear(in_features = 2048,out_features = self.embed_size)
+        self.linear_classifier = nn.Linear(in_features = self.hidden_size, out_features= self.categories)
 
+        self.embed_layer = nn.Embedding(self.vocab_size,self.embed_size)
+        self.gru = nn.GRU(input_size = self.embed_size ,hidden_size = self.hidden_size,num_layers = 1,batch_first = True)
+        self.bn = nn.BatchNorm1d(self.embed_size, momentum = 0.01)
+        self.linear_to_vocab = nn.Linear(in_features=self.hidden_size,out_features= self.vocab_size)
         
         
         
         
-    def forward(img):
-        self.h_init = torch.rand((self.num_layers,batch_size,256))
+        
+    def forward(self, img, captions,lengths):
+        self.h_init = torch.rand((1,batch_size,self.hidden_size))
         img = self.resnet(img)
         img = img.view(-1,2048*7*7)
 
        	img = F.relu(self.linear_layer(img))
        	img = self.dropout(img)
-       	img = F.relu(self.linear_layer_two(img))
+       	img = self.linear_layer_embed(img)
+        features = self.bn(img)
+        embeddings = self.embed_layer(captions)
+        embeddings = torch.cat((features.unsqueeze(1),embeddings), 1)
 
-       	input_seq = torch.autograd.Variable(torch.zeros(self.seq_len,self.batch_size,256), requires_grad = True)
-       	for i in range(input_seq.shape[0]):
-            input_seq[i,:,:] = img[:,:]
+       	packed = pack_padded_sequence(embeddings, lengths, batch_first=True) 
+        outputs, _ = self.gru(packed,self.h_init)
 
-        output, h_out = self.gru(input_seq,self.h_init)
-
-       	h_out = h_out.transpose(0,1)
+       	"""h_out = h_out.transpose(0,1)
         print(h_out.size())
        	h_out = h_out.view(-1,self.num_layers*self.hidden_size)
         print(h_out.size())
        	h_out = F.softmax(self.linear_classifier(h_out))
+        """
+        outputs_fin = self.linear_to_vocab(outputs[0])
 
-       	return output,h_out
+       	return outputs_fin
 
 
+
+
+def train(model,data_loader,epochs = 10,checkpoint = 10):
+
+  model.train()
+  loss_criterion = nn.CrossEntropyLoss()
+  
+  optimizer = optim.Adam(model.parameters(),lr = 0.0002)
+  total_step = len(data_loader)
+
+  for epoch in range(epochs):
+
+    for i,(images,captions,lengths) in enumerate(data_loader):
+
+       targets = pack_padded_sequence(captions, lengths, batch_first=True)[0]
+       outputs = model(images,captions,lengths)
+       loss = loss_criterion(outputs,targets)
+
+       decoder.zero_grad()
+       encoder.zero_grad()
+       loss.backward()
+       optimizer.step()
+
+       if i%5==0:
+        print('Epoch--->[{}/{}], Step--->[{}/{}], Loss--->{}'.format(epoch+1,epochs,i,total_step,loss.item()))
+
+    if (epoch+1)%checkpoint==0:
+      torch.save(model.state_dict(),MODEL_PATH)
+
+  
 
 def preprocess_img(image):
     normalize = transforms.Normalize(
@@ -74,7 +118,6 @@ def preprocess_img(image):
             normalize
             ])
     return preprocess(image)
-
 
 
 
